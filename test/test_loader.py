71ad09a89d34791a1e146a50281145d273c883be
393894082 <393894082@qq.com>
2019-08-08 04:30:44 +0800

    add loader file for loading file into memory

---
+++ test/test_loader.py
@@ -0,0 +1,17 @@
+import unittest
+
+"""
+    2019年8月8日 星期四 
+    本类主要测试文件加载功能
+    1. 加载单个指定格式文件进入内存
+    2. 加载整个目录下所有指定格式文件进入内存
+"""
+
+
+class LoaderTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_read_single_format_file(self):
+        pass
