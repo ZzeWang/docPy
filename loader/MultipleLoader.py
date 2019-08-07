f96ce6df9f08647eaecb34b3ef27aa0f6f750321
393894082 <393894082@qq.com>
2019-08-08 06:23:51 +0800

    add MultipFileLoader.py for loading mul files once time

---
+++ loader/MultipleLoader.py
@@ -0,0 +1,49 @@
+"""
+    dependencies
+"""
+import os
+import re
+import logging
+import threading
+from .SingleLoader import SingleFileLoader
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+"""
+    加载目录下所有非文件夹文件
+"""
+
+
+class MultipleFileLoader:
+
+    def __init__(self):
+        self._files_lib_path = ""
+        self._files = []
+        self._files_cnt = 0
+        self._loaded_file = []  # SingleFileLoader
+
+    def set_attr(self, lib):
+        self._files_lib_path = lib
+
+    def load(self):
+        try:
+            for path in os.listdir(self._files_lib_path):
+                if not os.path.isdir(path):
+                    single = SingleFileLoader()
+
+                    ppath = self._files_lib_path + '\\' + path
+                    nname = os.path.basename(path)
+                    fformat = os.path.splitext(nname)[-1][1:]
+                    single.set_attr(ppath, nname, fformat)
+                    self._loaded_file.append(threading.Thread(target=single.load))
+                else:
+                    logging.info("drop a dir with path={}".format(path))
+        except FileNotFoundError as f_not_e:
+            logging.fatal("path = {} not exists".format(self._files_lib_path))
+
+        try:
+            for sing in self._loaded_file:
+                sing.start()
+        except FileExistsError as e:
+            logging.fatal(e.errno)
