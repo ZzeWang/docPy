f6089e3ce2c39255e4b913923ab78a4396049f53
393894082 <393894082@qq.com>
2019-08-11 06:25:43 +0800

    add BASE class FileLoader for AbstractParser. And add a threading list to hold all load file threading for joining

--- loader/multipleLoader.py
+++ loader/multipleLoader.py
@@ -5,7 +5,7 @@ import os
 import re
 import logging
 import threading
-from .SingleLoader import SingleFileLoader
+from .SingleLoader import SingleFileLoader, FileLoader
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - MultipleFileLoader - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -15,12 +15,10 @@ logger = logging.getLogger(__name__)
 """
 
 
-class MultipleFileLoader:
+class MultipleFileLoader(FileLoader):
 
-    def __init__(self):
-        self._files_lib_path = ""
-        self._files = []
-        self._files_cnt = 0
+    def __init__(self, path=""):
+        self._files_lib_path = path
         self._loaded_file = []  # SingleFileLoader
 
     """
@@ -29,7 +27,8 @@ class MultipleFileLoader:
         :param lib : the group for files
         :return : void
     """
-    def set_attr(self, lib):
+
+    def set_attr_by_path(self, lib):
         self._files_lib_path = lib
 
     """
@@ -39,24 +38,29 @@ class MultipleFileLoader:
         :param:void
         :return:void
     """
+
     def load(self):
+        threadings = []
         try:
             for path in os.listdir(self._files_lib_path):
                 if not os.path.isdir(path):
                     single = SingleFileLoader()
-
                     ppath = self._files_lib_path + '\\' + path
                     nname = os.path.basename(path)
                     fformat = os.path.splitext(nname)[-1][1:]
                     single.set_attr(ppath, nname, fformat)
-                    self._loaded_file.append(threading.Thread(target=single.load, name=nname))
+                    self._loaded_file.append(single)
+                    threadings.append(threading.Thread(target=single.load, name=single._file_name))
                 else:
                     logging.info("drop a dir with path={}".format(path))
         except FileNotFoundError as f_not_e:
+            logging.fatal(f_not_e)
             logging.fatal("path = {} not exists".format(self._files_lib_path))
 
         try:
-            for sing in self._loaded_file:
+            for sing in threadings:
                 sing.start()
+            for sing in threadings:
+                sing.join()
         except FileExistsError as e:
             logging.fatal(e.errno)
