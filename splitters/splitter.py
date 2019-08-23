cb8942640b1ae5bbd70b736f5b6b5bfe9802d2cc
393894082 <393894082@qq.com>
2019-08-23 18:16:39 +0800

    add splitter.py for splitting a doc by some speclifical rule

---
+++ splitters/splitter.py
@@ -0,0 +1,13 @@
+"""
+    !: Splitter
+    $: 分割器，根据不同的范围分割文档，
+    $: 例如可以根据每个不同的模块分割文档，也可以根据不同的类分割文档
+    LK: docPy
+"""
+import dataclasses, logging
+
+
+
+@dataclasses.dataclass
+class Splitter:
+    pass
